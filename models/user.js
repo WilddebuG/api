e85ed60be19aa3bbac7d16fbd3b86605fa8f86d2
wilddebug <andriy.miga@gmail.com>
2019-11-14 18:28:24 +0200

    auth

--- models/user.js
+++ models/user.js
@@ -1,18 +1,22 @@
 'use strict';
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
         email: DataTypes.STRING,
-        password: DataTypes.STRING,
+        salt: DataTypes.STRING,
+        hash: DataTypes.STRING,
+        exp: DataTypes.INTEGER,
         companyId: {
             type: DataTypes.INTEGER,
             validate: {
-                isNumeric: true
+                isNumeric: true,
             }
         },
         status: {
             type: DataTypes.INTEGER,
             validate: {
-                isNumeric: true
+                isNumeric: true,
             }
         },
         token: DataTypes.STRING
@@ -24,6 +28,34 @@ module.exports = (sequelize, DataTypes) => {
         User.hasOne(models.Profile, {
             foreignKey: 'user_id',
         });
+    };
+    User.methods.setPassword = function (password) {
+        this.salt = crypto.randomBytes(16).toString('hex');
+        this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    };
+    User.methods.validatePassword = function(password) {
+        const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+        return this.hash === hash;
+    };
+
+    User.methods.generateJWT = function() {
+        const today = new Date();
+        const expirationDate = new Date(today);
+        expirationDate.setDate(today.getDate() + 60);
+
+        return jwt.sign({
+            email: this.email,
+            id: this._id,
+            exp: parseInt(expirationDate.getTime() / 1000, 10),
+        }, 'secret');
+    };
+
+    User.methods.toAuthJSON = function() {
+        return {
+            _id: this._id,
+            email: this.email,
+            token: this.generateJWT(),
+        };
     };
     return User;
 };
